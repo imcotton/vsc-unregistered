328a4ac4d15d1b7c780c8c694df2590ddae1d8c6
Cotton Hou <himcotton@gmail.com>
2018-11-04 14:09:57 +0800

    update extension code

--- extension.js
+++ extension.js
@@ -1,30 +1,88 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+'use strict';
+
+// @ts-check
+
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "unregistered" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+
+exports.activate = function (context) {
+    context.subscriptions.push(Controller(1000));
+};
+
+exports.deactivate = function () {
+};
+
+
+
+
+
+const link = 'https://code.visualstudio.com/buy';
+
+const message = [
+
+    'Hello! Thanks for trying out VS Code.',
+
+    '\n\n',
+
+    'This is an unregistered evaluation version',
+    ', and although the trial is untimed',
+    ', a license must be purchased for continued use.',
+
+    '\n\n',
+
+    'Would you like to purchase a license now?',
+
+].join('');
+
+
+
+
+
+function Controller (MAX = 42) {
+
+    let count = 0;
+
+    const subscriptions = vscode.Disposable.from(
+        vscode.window.onDidChangeTextEditorSelection(onEvent),
+    );
+
+
+    return Object.freeze({
+        dispose,
     });
 
-    context.subscriptions.push(disposable);
-}
-exports.activate = activate;
+    // ========================================================================
 
-// this method is called when your extension is deactivated
-function deactivate() {
+    function dispose () {
+        subscriptions.dispose();
+    }
+
+    function onEvent (event) {
+
+        if (event.kind !== vscode.TextEditorSelectionChangeKind.Keyboard) {
+            return;
+        }
+
+        count += 1;
+
+        if (count < MAX) {
+            return;
+        }
+
+        const prompt = vscode.window.showWarningMessage(message, { modal: true }, 'Purchase');
+
+        count = 0;
+
+        prompt.then(resolution => {
+            if (resolution === 'Purchase') {
+                vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(link));
+            }
+        });
+
+    }
+
 }
-exports.deactivate = deactivate;
+
